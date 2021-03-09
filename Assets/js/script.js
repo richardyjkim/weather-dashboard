821161647993c48de95631745686c8885b9d5700
Richard <richardyjkim@gmail.com>
2021-03-08 16:34:14 -0800

    searched city list/ display previous searched

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -11,9 +11,10 @@ let formSubmitHandler = function (event) {
   if (cityName) {
     getWeatherInfo(cityName);
     getFivedayInfo(cityName);
-    weatherContainerEl.innerHTML = "";
     cityTermEl.value = "";
-    searchedCities.push(cityName);
+    if (!searchedCities.includes(cityName)) {
+      searchedCities.push(cityName);
+    }
     localStorage.setItem("cityname", JSON.stringify(searchedCities));
     displaySearchCity();
   } else {
@@ -25,16 +26,18 @@ displaySearchCity = function () {
   cityListEl.textContent = "";
   for (let i = 0; i < searchedCities.length; i++) {
     let cityList = document.createElement("div")
-    cityList.classList = "card citylist flex-row justify-space-evenly";
+    cityList.classList = "citylist flex-row justify-space-evenly";
     cityList.setAttribute("value", searchedCities[i]);
     cityList.textContent=searchedCities[i];
     cityList.addEventListener("click", function () {
       getWeatherInfo(searchedCities[i]);
+      getFivedayInfo(searchedCities[i]);
     })
-    console.log(searchedCities[i]);
     cityListEl.appendChild(cityList);
   }
 }
+
+
 
 let getWeatherInfo = function (cityName) {
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
@@ -67,14 +70,21 @@ let getFivedayInfo = function (cityName) {
 };
 
 
-let displayWeather = function (data, cityName) {
-  //  create container for current city weather
-  let weatherEl = document.createElement("div");
-  let weatherInfoEl = document.createElement("div");
-  weatherEl.classList = "container flex-row justify-space-between align-center";
+displaySearchCity();
+if(searchedCities.length > 0) {
+  getWeatherInfo(searchedCities[searchedCities.length - 1]);
+  getFivedayInfo(searchedCities[searchedCities.length -1]);
+}
 
+
+
+let displayWeather = function (data, cityName) {
+  weatherContainerEl.classList.remove("display-none");
+  //  create container for current city weather
+  let weatherEl = document.getElementById("weatherEl");
+  let weatherInfoEl = document.getElementById("weatherInfoEl");
   // create a element to hold current city name
-  let cityTitleEl = document.createElement("h2");
+  let cityTitleEl = document.getElementById("cityTitleEl");
   // current time in string
   data.dt = new Date().toLocaleDateString();
   // get icon
@@ -84,33 +94,25 @@ let displayWeather = function (data, cityName) {
   getWeatherIconEl.setAttribute("src", getWeatherIcon);
   cityTitleEl.textContent = data.name + " (" + data.dt + ")";
   cityTitleEl.appendChild(getWeatherIconEl);
-  weatherContainerEl.appendChild(cityTitleEl);
-  cityTitleEl.classList = "container flex-row align-center";
 
   // get weather info
-  // temp
-  let cityTemperatureInfoEl = document.createElement("div");
+
+  let cityTemperatureInfoEl = document.getElementById("cityTemperatureInfoEl");
   let currentTemperature = Math.floor((data.main.temp - 273.15) * 1.8) + 32 + "°F";
-  cityTemperatureInfoEl.textContent = "Temperature: " + currentTemperature;
-  weatherInfoEl.appendChild(cityTemperatureInfoEl);
-  cityTemperatureInfoEl.classList = "margin-bottom flex-row justify-space-between align-center";
+  cityTemperatureInfoEl.textContent = "Temperature: " + currentTemperature; 
 
   // humid
-  let cityHumidityInfoEl = document.createElement("div");
+  let cityHumidityInfoEl = document.getElementById("cityHumidityInfoEl");
   let currentHumidity = data.main.humidity;
   cityHumidityInfoEl.textContent = "Humidity: " + currentHumidity + "%";
-  weatherInfoEl.appendChild(cityHumidityInfoEl);
-  cityHumidityInfoEl.classList = "margin-bottom flex-row justify-space-between align-center";
-
+  
   // wind Speed
-  let cityWindSpeedEl = document.createElement("div");
+  let cityWindSpeedEl = document.getElementById("cityWindSpeedEl");
   let currentWindSpeed = data.wind.speed;
   cityWindSpeedEl.textContent = "Wind Speed: " + currentWindSpeed + "MPH";
-  weatherInfoEl.appendChild(cityWindSpeedEl);
-  cityWindSpeedEl.classList = "margin-bottom flex-row justify-space-between align-center";
-
+  
   // UV index
-  let cityUvIndexEl = document.createElement("span");
+  let cityUvIndexEl = document.getElementById("cityUvIndexEl");
   let lat = data.coord.lat;
   let lon = data.coord.lon;
   let UvUrl = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${API_KEY}`
@@ -120,7 +122,6 @@ let displayWeather = function (data, cityName) {
       response.json().then(function (data) {
         let currentUvIndex = data.value;
         cityUvIndexEl.textContent = "UV Index: " + currentUvIndex;
-        weatherInfoEl.appendChild(cityUvIndexEl);
       });
     } else {
       cityUvIndexEl.textContent = "UV Index: Unkown";
@@ -128,21 +129,19 @@ let displayWeather = function (data, cityName) {
   }).catch(function (e) {
     cityUvIndexEl.textContent = "UV Index: Unkown";
   });
-  cityUvIndexEl.setAttribute("class", "badge badge-danger");
-  // append to container
-  weatherEl.appendChild(weatherInfoEl)
-  weatherContainerEl.appendChild(weatherEl);
 };
 
 // display 5 days
 let displayFivedays = function (data, cityName) {
   let fivedayEl = document.querySelectorAll(".fiveday");
+  let fivedayHeaderEl = document.getElementById("fivedayheader")
+  fivedayHeaderEl.textContent = "5-Day Forecast: " + data.city.name;
   for (let i = 0; i < fivedayEl.length; i++) {
     fivedayEl[i].textContent = "";
     // fivedays dates
     let fivedaysDateEl = document.createElement("p");
     let fivedaysIndex = i * 8 + 4;
-    let fivedaysDate = new Date(data.list[fivedaysIndex].dt);
+    let fivedaysDate = new Date(data.list[fivedaysIndex].dt * 1000);
     let fiveDay = fivedaysDate.toLocaleDateString();
     fivedaysDateEl.setAttribute("class", "mt-3 mb-0");
     fivedaysDateEl.textContent = fiveDay;
